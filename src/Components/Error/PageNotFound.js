46902965f891d9ef15c2adef5a0448c824e2e0a3
harish b <harish.balasubramanian15@siesgst.ac.in>
2022-05-16 00:05:04 -0400

    404 added

--- src/Components/Error/PageNotFound.js
+++ src/Components/Error/PageNotFound.js
@@ -1,8 +1,16 @@
+import {Stack, Typography} from "@mui/material";
+import homeStyle from "../Home/css/home.module.css";
+
 const PageNotFound = () => {
     return (
-        <div>
-            404|page not found
-        </div>
-    )
+
+        <Stack className={homeStyle.radialBG} direction={"column"} style={{
+            textAlign: "center", display: "flex", justifyContent: "center", alignItems: 'center',
+        }}>
+            <img src={'/404.png'} alt={"Loading"} width={"45%"}/>
+            <Typography variant={"h6"} component={"p"} marginTop={"20px"}>
+                Page not found
+            </Typography>
+        </Stack>)
 }
 export default PageNotFound
